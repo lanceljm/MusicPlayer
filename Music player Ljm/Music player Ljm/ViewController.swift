9522e5c4ddcd19b4834a2e66fc569ce4cd1da0f2
ljmlance <ljm2423124756@icloud.com>
2017-05-26 15:56:53 +0800

    项目初始化

---
+++ Music player Ljm/Music player Ljm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music player Ljm
+//
+//  Created by ljm on 2017/5/26.
+//  Copyright © 2017年 ljm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
